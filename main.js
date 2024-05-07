edbe40606f5339ce2e92af867a481a75ac680c14
Olha Danylevska <odanilevskaya2@gmail.com>
2024-05-07 19:43:13 +0100

    upload_file_path
    can see the whole path to the file, need to make it work

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require("path"); // Import the path module
+
+
+
+ipcMain.on('open-file-dialog', async (event) => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+
+  if (!result.canceled) {
+    const filePath = result.filePaths[0];
+    event.sender.send('file-selected', filePath);
+  }
+});
+
+// Your other code for creating windows, etc.
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  });
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js'), // Use the path module to construct the file path
+      sandbox: false
+    }
+  })
 
   win.loadURL("http://localhost:3000");
-
   win.webContents.openDevTools();
 };
 
--- src/App.js
+++ src/App.js
@@ -1,64 +1,105 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, openFileDialog } from 'react';
+import { useDropzone } from 'react-dropzone';
+
 import './App.css';
 import Table from './Table';
 import { ExcelContext, ChangesContext } from './ExcelData/ExcelContext';
 
+
 function App() {
-  const [input, setInput] = useState("/home/anna/Documents/my docs/case tracker.xltx");
+  // const [input, setInput] = useState("/home/anna/Documents/my docs/case tracker.xltx");
+  const [changes, setChanges] = useState([])
+  const [input, setInput] = useState("")
   const [tableData, setTableData] = useState([
     [
-      "no data uploaded"      
+      "no data uploaded"
     ],
   ]);
-  const [changes, setChanges] = useState([])
-  
+  const [filePaths, setFilePaths] = useState([])
+
+
+
   useEffect(() => {
-      }, [tableData]); 
- 
-    function readTheFile () {
-      const encodedInput = encodeURIComponent(input);
-      fetch(`http://localhost:5000/${encodedInput}`)
-        .then((response) => response.json())
-        .then((data) => {
+  }, [tableData]);
+
+  useEffect(() => {
+    readTheFile(filePaths);
+    setInput(filePaths)
+  }, [filePaths]);
+
+  function readTheFile(input) {
+    const encodedInput = encodeURIComponent(input);
+    fetch(`http://localhost:5000/${encodedInput}`)
+      .then((response) => response.json())
+      .then((data) => {
         setTableData(data);
         console.log(data)
-        })
-        .catch((error) => console.log(error));
-      }
-      
+      })
+      .catch((error) => console.log(error));
+  }
 
-      function handleSubmitButton(event) {
-        event.preventDefault();
-        console.log("submitted")
-        const encodedInput = encodeURIComponent(input);
-        fetch(`http://localhost:5000/${encodedInput}`, {
-          method: "PUT", // or 'PUT'
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(changes),
-        })
-          .then((response) => {
-            console.log("Status:", response.status);
-            setChanges([]);
-          }      
-             
-          )
-          .catch((error) => console.log(error));
+
+  function handleSubmitButton(event) {
+    event.preventDefault();
+    console.log("submitted")
+    const encodedInput = encodeURIComponent(input);
+    fetch(`http://localhost:5000/${encodedInput}`, {
+      method: "PUT", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(changes),
+    })
+      .then((response) => {
+        console.log("Status:", response.status);
+        setChanges([]);
       }
+
+      )
+      .catch((error) => console.log(error));
+  }
+
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles.length) {
+      setFilePaths(acceptedFiles.map((f) => '"' + f.path + '"'))
+    }
+    else {
+      setFilePaths(['Error'])
+    }
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="enterLocation">
-          <label>
+          <div className="container">
+            <p style={{ marginBottom: '50px' }}>
+              File paths:
+              <ul>
+                {filePaths.map((fp, i) => (
+                  <li key={i}>{fp}</li>
+                ))}
+              </ul>
+            </p>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <p>Drag n drop some files here, or click to select files</p>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* <label>
             Enter file location:{" "}
             <input value={input} onChange={(e) => setInput(e.target.value)} />
           </label>
           <button type="submit" onClick={(e) => readTheFile()}>
             Enter
           </button>
-        </div>
+        </div> */}
         <div className="saveData">
           <button type="submit" onClick={handleSubmitButton}>
             Save changes
@@ -67,7 +108,7 @@ function App() {
         <div className="manageData"></div>
       </header>
       <ExcelContext.Provider value={{ tableData, setTableData }}>
-        <ChangesContext.Provider value={{ changes, setChanges}}>
+        <ChangesContext.Provider value={{ changes, setChanges }}>
           <Table table={tableData} />
         </ChangesContext.Provider>
       </ExcelContext.Provider>
